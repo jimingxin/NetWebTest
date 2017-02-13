daa35c16b0ab2a0264899ca97856e098e9107e30
嵇明新 <493972613@qq.com>
2017-02-13 15:27:01 +0800

    NSUrlSession和AFNetWorking 基础操作

--- NetWorkTest/AppDelegate.m
+++ NetWorkTest/AppDelegate.m
@@ -18,6 +18,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     // Override point for customization after application launch.
     ViewController *mainViewController = [[ViewController alloc] init];
     UINavigationController *navigationController = [[UINavigationController alloc] initWithRootViewController:mainViewController];
